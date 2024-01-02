049a6b0296c7e4a76716073b597fc38adfcbb9e4
DannyTan8x <dannytan8xtw@icloud.com>
2024-01-02 23:16:48 +0800

    updated jQuery/Swipper/LightBox

--- Project2_jQuery/js/all.js
+++ Project2_jQuery/js/all.js
@@ -5,4 +5,49 @@ $(document).ready(function(){
         // $('.text').toggle();
         $('.text').fadeToggle();
     });
-})
+
+    $('.top').click(function(event){
+        event.preventDefault();
+        $('html,body').animate({
+          scrollTop: 0
+        },1000);
+      });
+
+
+
+      $('.menu').click(function(event){
+        // event.preventDefault();
+            $(this).addClass('active').parent().siblings().find('.menu').removeClass('active');
+      });
+
+      lightbox.option({
+        'resizeDuration': 200,
+        'wrapAround': true
+      })
+
+    })
+    const swiper = new Swiper('.swiper', {
+      // autoplay
+      autoplay: {
+        delay: 3000,
+      },
+      // Optional parameters
+      // direction: 'vertical',
+      loop: true,
+    
+      // If we need pagination
+      pagination: {
+        el: '.swiper-pagination',
+      },
+    
+      // Navigation arrows
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    
+      // And if we need scrollbar
+      scrollbar: {
+        el: '.swiper-scrollbar',
+      },
+    });
