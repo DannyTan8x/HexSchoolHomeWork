dcc23ffda2127b739dbd2fb7a983c4aefaf78d00
DannyTan8x <dannytan8xtw@icloud.com>
2024-01-06 23:00:26 +0800

    update project1 homePage menu

--- Project2_jQuery/js/all.js
+++ Project2_jQuery/js/all.js
@@ -20,6 +20,22 @@ $(document).ready(function(){
             $(this).addClass('active').parent().siblings().find('.menu').removeClass('active');
       });
 
+      $('#showMenu').click(function(event){
+        event.preventDefault();
+        console.log('clicked');
+        $('.homeMenu').css("transform", "translateX(0%)");
+        $('#hideMenu').css("display", "block");
+        $(this).css("display", "none");
+      });
+
+      $('#hideMenu').click(function(event){
+        event.preventDefault();
+        console.log('clicked');
+        $('.homeMenu').css("transform", "translateX(-80%)");
+        $('#showMenu').css("display", "block");
+        $(this).css("display", "none");
+      });
+
       lightbox.option({
         'resizeDuration': 200,
         'wrapAround': true,
@@ -27,7 +43,10 @@ $(document).ready(function(){
         'maxHeight': 500,
       })
 
+
     })
+
+
     const swiper = new Swiper('.swiper', {
       // autoplay
       autoplay: {
