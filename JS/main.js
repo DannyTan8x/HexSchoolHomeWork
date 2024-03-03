1a6def9f4e20898eec9ea26f3ad97dba5459a8e6
DannyTan8x <dannytan8xtw@icloud.com>
2024-03-03 21:35:31 +0800

    updated

--- JS/main.js
+++ JS/main.js
@@ -48,7 +48,7 @@ ScrollReveal({
 });
 
 ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
-ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form, .cert_box', { origin: 'bottom' });
 ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
 ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
